390fee18b75140519fa303ca8c920efe9bb0da83
dict1034 <dictegeertinger@gmail.com>
2020-05-26 16:03:36 +0200

    burgermenu virker på mobil (kontakt.html)

---
+++ burgermenu.js
@@ -0,0 +1,13 @@
+let click = 0;
+
+document.querySelector('.wrapper-menu').addEventListener('click', function () {
+    document.querySelector('.wrapper-menu').classList.toggle('open');
+    click++;
+
+    if (click == 1) {
+        document.querySelector("#mobil_menu").style.display = "flex";
+    } else if (click == 2) {
+        document.querySelector("#mobil_menu").style.display = "none";
+        click = 0;
+    }
+});
