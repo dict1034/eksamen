9faf6499001bcf17003e982cb520ff8dfe3f3ad1
drjurgen <nicoj98@gmail.com>
2020-05-30 15:15:21 +0200

    burgermenu tilpasset til screen resizing

--- burgermenu.js
+++ burgermenu.js
@@ -3,11 +3,26 @@ let click = 0;
 document.querySelector('#burgermenu').addEventListener('click', function () {
     document.querySelector('.wrapper-menu').classList.toggle('open');
     click++;
+    checkWindowSize();
+});
 
-    if (click == 1) {
-        document.querySelector("#mobil_menu").style.display = "flex";
-    } else if (click == 2) {
+
+let closeMenuOnBigScreen = window.matchMedia("(max-width: 768px)");
+closeMenuOnBigScreen.addListener(checkWindowSize);
+
+function checkWindowSize() {
+    if (closeMenuOnBigScreen.matches) {
+        if (click == 1) {
+            document.querySelector("#mobil_menu").style.display = "flex";
+        } else if (click == 2) {
+            document.querySelector("#mobil_menu").style.display = "none";
+            click = 0;
+        }
+    } else {
         document.querySelector("#mobil_menu").style.display = "none";
+        if (click == 1) {
+            document.querySelector('.wrapper-menu').classList.toggle('open');
+        }
         click = 0;
     }
-});
+}
